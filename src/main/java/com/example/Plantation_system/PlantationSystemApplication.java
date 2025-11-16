289ce37cae40cd2f4a19670d9bbf3da3f9ee49c5
M-Taha90 <mtahaimran3@gmail.com>
2025-11-16 17:20:56 +0500

    Initial Spring Boot project commit

---
+++ src/main/java/com/example/Plantation_system/PlantationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Plantation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantationSystemApplication.class, args);
+	}
+
+}
