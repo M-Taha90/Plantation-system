1c206851a2ba23ea68e06e8ed61ebbc7629cd4d2
M-Taha90 <mtahaimran3@gmail.com>
2025-11-18 12:41:22 +0500

    repositories code added

--- src/main/java/com/example/Plantation_system/PlantationSystemApplication.java
+++ src/main/java/com/example/Plantation_system/PlantationSystemApplication.java
@@ -1,7 +1,11 @@
 package com.example.Plantation_system;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PlantationSystemApplication {
@@ -9,5 +13,11 @@ public class PlantationSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlantationSystemApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner testDb(DataSource ds) {
+    	return args -> System.out.println("DB connected: " + ds.getConnection().getMetaData().getDatabaseProductName());
+	}
+
 
 }
